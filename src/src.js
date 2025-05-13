338648be39cdd9db4f86e583ee5b12abeae846ef
전선일 <jeonsunil1225@gamil.com>
2025-05-13 16:31:56 +0900

    자기소개서 부분 보기 좋게 조금 수정, 만들어 놓은 데이터에서 뽑아와 경험 부분 표로 뽑아옴.

--- src/src.js
+++ src/src.js
@@ -53,80 +53,97 @@ const linusInfo = {
   experience : [
     {
       name : `그린아트컴퓨터학원 대전둔산점`,
-      detail : `- 직업 훈련 과정 명 : 기업에서 요구하는 프레임워크를 활용한 풀스택 개발자(NODE,ECMAscript)양성
-      <br>- 프로젝트 :
-      <br>1. 비피하기 게임 사이트 (팀 프로젝트)
-      <br>2. 리그오브레전드 밴픽 사이트 (팀 프로젝트)
-      <br>3. 업다운 게임 사이트 (팀 프로젝트)`,
+      detail : [
+        {main : `- 직업 훈련 과정 명 : 기업에서 요구하는 프레임워크를 활용한 풀스택 개발자(NODE,ECMAscript)양성`},
+      {main : `- 프로젝트 :`},
+      {main : `1. 비피하기 게임 사이트 (팀 프로젝트)`},
+      {main : `2. 리그오브레전드 밴픽 사이트 (팀 프로젝트)`},
+      {main : `3. 업다운 게임 사이트 (팀 프로젝트)`},],
       date : `2024.12 ~ 2025.07`
     },
     {
       name : `세종특별자치시 제 10선거구 (종촌동) 시의회 의원 후보자 손웅 사무소`,
-      detail : `- 당시 아르바이트 하던 편의점의 사장님께서 제8회 전국동시지방선거에 시의원 후보로 나가게 되심. 
-      <br>- '너는 믿을 수 있다'고 하시며, 아르바이트 한다고 생각하고, 사무장을 맡아달라고 하심.
-      <br>- 기반 지식은 없었지만, 선거위원회에서 나누어진 유인물과, 선거위원회에서 개최한 강연에 참석하며, 업무를 해 나감
-      <br>- 이 경험을 토대로 모르는 것을 학습하는데에 두려움을 극복하고, 유하게 받아들이게 되는 자세를 가지게 되었음.
-      <br>- 결과적으로는 탈락하게 되었지만 42.50%의 득표율을 얻어내었던 경험을 얻게 되었음.`
-    },
+      detail : [
+      { main: `- 당시 아르바이트 하던 편의점의 사장님께서 제8회 전국동시지방선거에 시의원 후보로 나가게 되심.`},
+      { main: `- '너는 믿을 수 있다'고 하시며, 아르바이트 한다고 생각하고, 사무장을 맡아달라고 하심.`},
+      { main: `- 기반 지식은 없었지만, 선거위원회에서 나누어진 유인물과, 선거위원회에서 개최한 강연에 참석하며, 업무를 해 나감`},
+      { main: `- 이 경험을 토대로 모르는 것을 학습하는데에 두려움을 극복하고, 유하게 받아들이게 되는 자세를 가지게 되었음.`},
+      { main: `- 결과적으로는 탈락하게 되었지만 42.50%의 득표율을 얻어내었던 경험을 얻게 되었음.`}],
+      date: `2022.04 ~ 2022.06`,
+      },
     {
       name : `gs25 세종 래미안점`,
-      detail : `- 손님 응대
-                <br>- 물품 판매
-                <br>- 창고 정리
-                <br>- 물류 정리
-                <br>- 매대 정리
-                <br>- 점포 청소
-                <br>- 사회복무요원 복무이행으로 인한 퇴직.`,
+      detail :  [
+        {main: `- 손님 응대`},
+        {main: `- 물품 판매`},
+        {main: `- 창고 정리`},
+        {main: `- 물류 정리`},
+        {main: `- 매대 정리`},
+        {main: `- 점포 청소`},
+        {main: `- 사회복무요원 복무이행으로 인한 퇴직.`}
+      ],
       date : `2021.03 ~ 2023.02`
     },
     {
       name : `백석대학교/상담학과 과임원`,
-      detail : `- 과임원 대외협력부장을 역임함.
-                <br>- 과에서 행사를 할 때, 장소를 섭외하고, 인원을 섭외함.
-                <br>- 리더쉽을 발휘해서, 과 사람들을 이끔.`,
+      detail : [
+        {main: `- 과임원 대외협력부장을 역임함.`},
+        {main: `- 과에서 행사를 할 때, 장소를 섭외하고, 인원을 섭외함.`},
+        {main: `- 리더쉽을 발휘해서, 과 사람들을 이끔.`},
+              ],
       date : `2020.01 ~ 2021.02`
     },
     {
       name : `백석대학교/ 두드림:대학생도박문제예방활동단`,
-      detail : `- 한국도박문제관리센터 와 협업 하여 대학생 도박예방 캠페인 진행
-                <br>- 2020년 대학생 도박문제 예방활동단 평가대회 최우수상 수상
-                <br>- 해당 기사 링크 https://www.newsway.co.kr/news/view?ud=2020111617274993757`,
+      detail : [
+        {main: `- 한국도박문제관리센터 와 협업 하여 대학생 도박예방 캠페인 진행`},
+        {main: `- 2020년 대학생 도박문제 예방활동단 평가대회 최우수상 수상`},
+        {main: `- 해당 기사 링크 https://www.newsway.co.kr/news/view?ud=2020111617274993757`},
+      ],
       date : `2019.03 ~ 2021.03`
     },
     {
       name : `레드존 PC & 코인노래연습장`,
-      detail : `- 손님 응대
-                <br>- 피시방 업무
-                <br>- 노래방 업무
-                <br>- 매점 청소
-                <br>- 물류 정리
-                <br>- 대학교에서 과 임원을 역임하게 되어 시간조정이 되지않아 퇴직.`,
+      detail : [
+        {main: `- 손님 응대`},
+        {main: `- 피시방 업무`},
+        {main: `- 노래방 업무`},
+        {main: `- 매점 청소`},
+        {main: `- 물류 정리`},
+        {main: `- 대학교에서 과 임원을 역임하게 되어 시간조정이 되지않아 퇴직.`}
+        ],
       date : `2019.04 ~ 2020.01`
     },
     {
       name : `맘스터치 세종새롬점`,
-      detail : `- 손님 응대
-                <br>- 치킨 조리
-                <br>- 햄버거 조리
-                <br>- 기름 정제
-                <br>- 매장 청소
-                <br>- 대학교에서 과 임원을 역임하게 되어 시간 조정이 되지않아 퇴직`,
+      detail : [
+        {main: `- 손님 응대`},
+        {main: `- 치킨 조리`},
+        {main: `- 햄버거 조리`},
+        {main: `- 기름 정제`},
+        {main: `- 매장 청소`},
+        {main: `- 대학교에서 과 임원을 역임하게 되어 시간 조정이 되지않아 퇴직`}
+      ],
       date : `2019.03 ~ 2020.01`
     },
     {
       name : `세종대성고등학교/축구 동아리`,
-      detail : `- 재학 중 3년간 세종시 스포츠 클럽 축구 대회 우승 2회, 준우승 2회, 풋살 대회 우승 1회, 준우승 1회
-                <br>- 2학년 시기에 풋살 시 대회 각각 우승 후, 풋살 전국 대회 출전
-                <br>- 2학년 시기에 학교 축구, 풋살 대표팀 주장 역임`,
+      detail : [
+        {main: `- 재학 중 3년간 세종시 스포츠 클럽 축구 대회 우승 2회, 준우승 2회, 풋살 대회 우승 1회, 준우승 1회`},
+        {main: `- 2학년 시기에 풋살 시 대회 각각 우승 후, 풋살 전국 대회 출전`},
+        {main: `- 2학년 시기에 학교 축구, 풋살 대표팀 주장 역임`}
+              ],
       date : `2015.03 ~ 2018.02`
     },
     {
       name : `세종어진중학교/밴드 동아리`,
-      detail : `- 3학년 시기 전학 온 후, 마음 맞는 친구들과 밴드 결성
-                - 음악 선생님을 고문으로 모셔 밴드 동아리 창설
-                - 밴드 동아리 보컬로 활동
-                - 학교 축제에서 '나는 나비', '꽃송이가' 2곡 공연
-                - 졸업식에서 '이젠 안녕' 1곡 공연`,
+      detail : [
+        {main: `- 3학년 시기 전학 온 후, 마음 맞는 친구들과 밴드 결성`},
+        {main: `- 음악 선생님을 고문으로 모셔 밴드 동아리 창설`},
+        {main: `- 밴드 동아리 보컬로 활동`},
+        {main: `- 학교 축제에서 '나는 나비', '꽃송이가' 2곡 공연`},
+        {main: `- 졸업식에서 '이젠 안녕' 1곡 공연`}
+              ],
       date : `2014.06 ~ 2015.02`
     }, 
   ],
@@ -165,11 +182,48 @@ console.log(linusInfo);
 
 console.log(Object.keys(linusInfo));
 
-Object.keys(linusInfo).forEach((element, i) => {
+async function infoMake() {
+await Object.keys(linusInfo).forEach((element, i) => {
   const div = document.createElement('div');
-  div.classList.add(`info_${Object.keys(linusInfo)[i]}`)
-  element = document.createElement('h2');
-  element.textContent = Object.keys(linusInfo)[i];
-  div.appendChild(element)
+  div.classList.add(`info_${element}`)
+  const h2 = document.createElement('h2');
+  h2.textContent = Object.keys(linusInfo)[i];
+
+  div.appendChild(h2);
   info.appendChild(div);
 });
+
+linusInfo.experience.forEach((element, i) => {
+  const experience = document.querySelector('.info_experience');
+
+  const table = document.createElement('table');
+  const tbody = document.createElement('tbody');
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+  const td2 = document.createElement('td');
+  const td2H2 = document.createElement('h2');
+
+  td.textContent = element.date;
+  td2H2.textContent = element.name;
+  td2.appendChild(td2H2);
+  // console.log(element.detail);
+  linusInfo.experience[i].detail.forEach((element) => {
+    // td2.textContent = element.main
+    // console.log(element.main);
+    const td2P = document.createElement('p');
+    td2P.textContent = element.main;
+
+    td2.appendChild(td2P);
+  });
+
+  tr.appendChild(td);
+  tr.appendChild(td2);
+  tbody.appendChild(tr);
+  table.appendChild(tbody);
+  experience.appendChild(table);
+})
+
+
+}
+
+infoMake();
